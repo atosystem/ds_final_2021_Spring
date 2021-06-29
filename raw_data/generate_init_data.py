bdd8d937b4a9e1901d217c2cf58be0198882b369
Liang-Yu Lu <liang-yulu@Liang-Yus-MacBook-Pro.local>
2021-06-29 12:52:06 +0800

    style: change the format if init_data

--- raw_data/generate_init_data.py
+++ raw_data/generate_init_data.py
@@ -1,16 +1,25 @@
 import json
 import random
 import codecs
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--infected_people_num', default=1000)
+args = parser.parse_args()
+#print(args.infected_people_num)
 with open('./all_place.json', newline='') as f:
     place = json.load(f)
 with open('./all_people.json', newline='') as f:
     people = json.load(f)
 
-data = dict()
+path = dict()
 for i in people["People"] :
-    data[i] = random.choice(place["Place"])
-
+    path[i] = random.choice(place["Place"])
+data = dict()
+data["people_list"] = path
+data["infected_people"] = []
+for i in range(int(args.infected_people_num)) :
+    data["infected_people"].append(random.choice(people["People"]))
 fp = codecs.open('init_data.json', 'a+', 'utf-8')
 fp.write(json.dumps(data,ensure_ascii=False))
 fp.close()
